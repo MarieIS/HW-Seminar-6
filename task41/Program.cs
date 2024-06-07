835ef5b169ab86813e257b876db3f0caab3d6601
Maria <mariya-ivanova@list.ru>
2024-06-07 15:35:26 +0200

    task41 was solved

---
+++ task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void GetCountAmount(int length)
+{
+   int count = 0;
+   for (int i = 0; i < length; i++)
+   {
+      Console.Write("Введите ваше "+(i+1)+" целое число: ");
+      int user_input = Convert.ToInt32(Console.ReadLine());
+      if (user_input > 0) 
+      {
+         count++;
+      }
+   }
+   Console.WriteLine($"Количество положительных чисел равно: {count}");
+}
+
+int GetNumberAmount(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetNumberAmount("Введите желаемое количество чисел: ");
+GetCountAmount(number);
