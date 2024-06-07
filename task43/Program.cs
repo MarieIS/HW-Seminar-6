ae97af52909aa5edbe48989b176743615cba6b16
Maria <mariya-ivanova@list.ru>
2024-06-07 23:01:59 +0200

    task43 was almost solved

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,17 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-void GetCountAmount(int length)
+float GetY(int coeff_k, int coeff_b, float x_coord)
 {
-   int count = 0;
-   for (int i = 0; i < length; i++)
-   {
-      Console.Write("Введите ваше "+(i+1)+" целое число: ");
-      int user_input = Convert.ToInt32(Console.ReadLine());
-      if (user_input > 0) 
-      {
-         count++;
-      }
-   }
-   Console.WriteLine($"Количество положительных чисел равно: {count}");
+   return (coeff_k * x_coord + coeff_b);
+}
+
+float GetX(int coeff_k1, int coeff_b1, int coeff_k2, int coeff_b2) /*k1x+b1=k2x+b2*/
+{
+   return (coeff_b2 - coeff_b1) / (coeff_k1 - coeff_k2);
 }
 
 int GetNumberAmount(string message)
@@ -24,4 +19,6 @@ int k1 = GetNumberAmount("Введите коэффициент k1: ");
 int b1 = GetNumberAmount("Введите коэффициент b1: ");
 int k2 = GetNumberAmount("Введите коэффициент k2: ");
 int b2 = GetNumberAmount("Введите коэффициент b2: ");
-GetCountAmount(number);
+float x = GetX(k1, b1, k2, b2);
+float y = GetY(k1, b1, x);
+Console.WriteLine($"Координаты точки пересечения двух прямых: {x} и {y}");
